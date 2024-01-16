000af335e44f2682073a660db99b44db2988e43b
kuldeep2745 <kuldeeprathore2745@gmail.com>
2024-01-16 21:03:43 +0530

    Created todo structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState("");
+  const [dataArray, setDataArray] = useState([]);
+  const [filtered, setFiltered] = useState("All");
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    setData(e.target.value);
+  };
+
+  const onClickHandler = () => {
+    if (editMode && editingIndex !== null) {
+      const updatedArray = dataArray.map((item, i) =>
+        i === editingIndex ? { ...item, text: data } : item
+      );
+      setDataArray(updatedArray);
+      setEditMode(false);
+      setEditingIndex(null);
+    } else {
+      setDataArray((prevValue) => [
+        ...prevValue,
+        { text: data, checked: false },
+      ]);
+    }
+    setData("");
+  };
+
+  const editHandler = (index) => {
+    setEditMode(true);
+    setEditingIndex(index);
+    setData(dataArray[index].text);
+  };
+
+  const checkHandler = (index) => {
+    const updatedArray = dataArray.map((item, i) =>
+      index === i ? { ...item, checked: !item.checked } : item
+    );
+    setDataArray(updatedArray);
+  };
+
+  const removeHandler = (index) => {
+    const filteredArray = dataArray.filter((item, i) => index !== i);
+    setDataArray(filteredArray);
+  };
+
+  const filterHandler = (param) => {
+    setFiltered(param);
+  };
+
+  const filteredArray = () => {
+    if (filtered === "Checked") {
+      return dataArray.filter((item) => item.checked);
+    } else if (filtered === "UnChecked") {
+      return dataArray.filter((item) => !item.checked);
+    } else {
+      return dataArray;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <input value={data} onChange={(e) => onChangeHandler(e)} />
+      <button onClick={onClickHandler}>{editMode ? "Edit" : "Add"}</button>
+      <br />
+      <button onClick={() => filterHandler("All")}>All</button>
+      <button onClick={() => filterHandler("Checked")}>Checked</button>
+      <button onClick={() => filterHandler("UnChecked")}>UnChecked</button>
+      {filteredArray()?.map((item, index) => (
+        <li
+          key={index}
+          style={{
+            textDecoration: `${item.checked ? "line-through" : "none"}`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="checkbox"
+            checked={item.checked}
+            onChange={() => checkHandler(index)}
+          />
+          {item.text}
+          <button onClick={() => editHandler(index)}>Edit</button>
+          <button onClick={() => removeHandler(index)}>X</button>
+        </li>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
